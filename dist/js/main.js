0a02b0339421a2f11840a08ee512d39e43b5aac7
Chelsea <chelsea.campbell@my.creativecirucs.edu>
2019-09-03 21:31:48 -0400

    get call setup

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,21 @@
 "use strict";
 
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log("hello, chello. GoogleFlickr1.0");
+var picSearch_button = document.querySelector("[name='picSearch_button']");
+var picSearch_query = document.querySelector("[name='SearchInputField']");
+
+(function () {
+  console.log('flicker function booted...');
+  var API_URL_BASE = 'https://www.flickr.com/services/rest/?method=flickr.photos.getPopular&api_key=47fa016833c10c7cf777062f48eb2908&user_id=184085204%40N06&format=json&nojsoncallback=1';
+
+  function makeSearchRequest(event) {
+    //changes value/calls getUserData()
+    //presents the browser from doing default behavior like refreshing/loading/ etc
+    event.preventDefault();
+    var photoSearch = picSearch_query.value;
+    console.log('value of photo query: ', photoSearch); // getQueryData(quickSearch)
+  }
+
+  picSearch_button.addEventListener("click", makeSearchRequest);
+})();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
-console.log(`Hello World from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+console.log("hello, chello. GoogleFlickr1.0");
+
+const picSearch_button = document.querySelector("[name='picSearch_button']");
+const picSearch_query = document.querySelector("[name='SearchInputField']")
+
+;(function(){
+    console.log ('flicker function booted...');
+    const API_URL_BASE= 'https://www.flickr.com/services/rest/?method=flickr.photos.getPopular&api_key=47fa016833c10c7cf777062f48eb2908&user_id=184085204%40N06&format=json&nojsoncallback=1';
+
+    function makeSearchRequest(event){ //changes value/calls getUserData()
+        //presents the browser from doing default behavior like refreshing/loading/ etc
+        event.preventDefault()
+        const photoSearch = picSearch_query.value
+        console.log('value of photo query: ', photoSearch)
+       // getQueryData(quickSearch)
+    }
+
+    picSearch_button.addEventListener("click", makeSearchRequest);
+})()
